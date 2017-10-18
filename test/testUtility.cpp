347c46bfbbf74d91438854ddedf9113527f8dc84
Harikrishnan <harikrishnan.prabakaran@zohocorp.com>
2017-10-18 20:20:04 +0530

    Add test for List

--- test/testUtility.cpp
+++ test/testUtility.cpp
@@ -9,7 +9,7 @@
 #include "Utility.h"
 #include "catch.hpp"
 
-TEST_CASE("Factorials are computed", "[factorial]")
+TEST_CASE("Freelist Test", "[utility]")
 {
 	using namespace SmallAlloc;
 
@@ -55,4 +55,58 @@ TEST_CASE("Factorials are computed", "[factorial]")
 	REQUIRE(static_cast<FLNode *>(fl.pop())->val == 5);
 	REQUIRE(static_cast<FLNode *>(fl.pop())->val == 4);
 	REQUIRE(fl.empty() == true);
+}
+
+TEST_CASE("List Test", "[utility]")
+{
+	using namespace SmallAlloc;
+
+	struct ListNode : public List::Node
+	{
+		long val;
+
+		ListNode() : val(0)
+		{}
+
+		ListNode(long v) : val(v)
+		{}
+	};
+
+	List list;
+
+	ListNode n1{1};
+	ListNode n2{2};
+	ListNode n3{3};
+
+	list.push_front(&n1);
+	REQUIRE(static_cast<ListNode *>(list.front())->val == 1);
+	REQUIRE(static_cast<ListNode *>(list.back())->val == 1);
+
+	list.push_front(&n2);
+	REQUIRE(static_cast<ListNode *>(list.front())->val == 2);
+	REQUIRE(static_cast<ListNode *>(list.back())->val == 1);
+
+	list.push_back(&n3);
+	REQUIRE(static_cast<ListNode *>(list.front())->val == 2);
+	REQUIRE(static_cast<ListNode *>(list.back())->val == 3);
+	REQUIRE(list.empty() == false);
+
+	REQUIRE(static_cast<ListNode *>(list.pop_front())->val == 2);
+	REQUIRE(static_cast<ListNode *>(list.pop_back())->val == 3);
+	REQUIRE(static_cast<ListNode *>(list.pop_back())->val == 1);
+	REQUIRE(list.empty() == true);
+
+	ListNode n4{4};
+	ListNode n5{5};
+
+	list.push_back(&n4);
+	list.push_back(&n5);
+	list.move_front(&n5);
+	REQUIRE(static_cast<ListNode *>(list.front())->val == 5);
+	list.move_front(&n4);
+	REQUIRE(static_cast<ListNode *>(list.front())->val == 4);
+	list.move_back(&n4);
+	REQUIRE(static_cast<ListNode *>(list.back())->val == 4);
+	list.move_back(&n5);
+	REQUIRE(static_cast<ListNode *>(list.back())->val == 5);
 }
