8f585398987f677a6e989c3ba252eb8829995664
Harikrishnan <harikrishnan.prabakaran@zohocorp.com>
2017-10-20 11:24:01 +0530

    Implement Buddy Allocator

--- src/BuddyManager.cpp
+++ src/BuddyManager.cpp
@@ -30,9 +30,10 @@ struct BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl
 {
 	BuddyManagerImpl();
 
-	static constexpr SizeClass get_size_class(Size size);
-	void *get_buddy(void *ptr, SizeClass szc);
+	static constexpr SizeClass get_sizeclass(Size size);
+
 	List &get_freelist(SizeClass szc);
+	void *get_buddy(void *ptr, SizeClass szc);
 	void mark_block_as_free(void *ptr, SizeClass szc);
 	void mark_block_as_in_use(void *ptr, SizeClass szc);
 	bool block_is_free(void *ptr, SizeClass szc);
@@ -45,20 +46,23 @@ private:
 	static constexpr size_t get_normalized_ptr(void *ptr);
 	static constexpr Index get_bitmap_index(void *ptr, SizeClass szc);
 
+	List m_freelist[get_num_sizeclasses()];
 	char m_bitmap[get_bitmap_size()];
-	List freelist[get_num_sizeclasses()];
 };
 
 
 template <size_t PageSize, size_t MinAllocSize>
-BuddyManager<PageSize, MinAllocSize>::BuddyManager(void *meta,
-												   void *chunk) : m_managed_chunk(chunk), m_impl(new (meta) BuddyManagerImpl())
-{}
+BuddyManager<PageSize, MinAllocSize>::BuddyManager(void *meta, void *chunk)
+	: m_managed_chunk(chunk), m_impl(new (meta) BuddyManagerImpl())
+{
+	auto freelist = m_impl->get_freelist(m_impl->get_sizeclass(PageSize));
+	freelist.push_front(m_managed_chunk);
+}
 
 template <size_t PageSize, size_t MinAllocSize>
 void *BuddyManager<PageSize, MinAllocSize>::alloc(size_t size)
 {
-	auto szc = m_impl->get_size_class(size);
+	auto szc = m_impl->get_sizeclass(size);
 	auto freelist = m_impl->get_freelist(szc);
 	void *ret_mem = freelist->pop_front();
 	void *buddy;
@@ -118,6 +122,12 @@ size_t log_2(size_t n)
 }
 
 template <size_t PageSize, size_t MinAllocSize>
+BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::BuddyManagerImpl()
+{
+	memset(m_bitmap, 0, get_bitmap_size());
+}
+
+template <size_t PageSize, size_t MinAllocSize>
 constexpr Count BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::get_num_sizeclasses()
 {
 	return log_2(PageSize) - log_2(MinAllocSize) + 1;
@@ -130,7 +140,7 @@ constexpr Size BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::get_bitma
 }
 
 template <size_t PageSize, size_t MinAllocSize>
-constexpr SizeClass BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::get_size_class(
+constexpr SizeClass BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::get_sizeclass(
 	Size size)
 {
 	assert(size >= MinAllocSize && size <= PageSize);
@@ -150,12 +160,63 @@ template <size_t PageSize, size_t MinAllocSize>
 constexpr Index BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::get_normalized_ptr(
 	void *ptr)
 {
-	return reinterpret_cast<size_t>(ptr) & (PageSize - 1);
+	return reinterpret_cast<size_t>(ptr) % PageSize;
 }
 
 template <size_t PageSize, size_t MinAllocSize>
 constexpr Index BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::get_bitmap_index(void *ptr,
 																						 SizeClass szc)
 {
-	return (1 << (get_num_sizeclasses() - (szc + 1))) - 1 + get_normalized_ptr(ptr) / get_size(szc);
+	return (1 << (get_num_sizeclasses() - (szc + 1))) - 1 + get_normalized_ptr(ptr) / get_size(szc) - 1;
+}
+
+template <size_t PageSize, size_t MinAllocSize>
+List &BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::get_freelist(SizeClass szc)
+{
+	return m_freelist[szc];
+}
+
+template <size_t PageSize, size_t MinAllocSize>
+void *BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::get_buddy(void *ptr, SizeClass szc)
+{
+	auto ptr_start = reinterpret_cast<size_t>(ptr) / PageSize;
+	auto n_ptr = reinterpret_cast<size_t>(ptr) % PageSize;
+
+	return reinterpret_cast<void *>(ptr_start + n_ptr ^ 0x1);
+}
+
+template <size_t PageSize, size_t MinAllocSize>
+void BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::mark_block_as_free(void *ptr,
+																				SizeClass szc)
+{
+	auto bitmap_index = get_bitmap_index(ptr, szc);
+	auto bitmap_byte = bitmap_index / 8;
+	auto bitmap_bit = bitmap_index % 8;
+
+	assert(!block_is_free(ptr, szc));
+
+	m_bitmap[bitmap_byte] &= ~(1 << bitmap_bit);
+}
+
+template <size_t PageSize, size_t MinAllocSize>
+void BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::mark_block_as_in_use(void *ptr,
+																				  SizeClass szc)
+{
+	auto bitmap_index = get_bitmap_index(ptr, szc);
+	auto bitmap_byte = bitmap_index / 8;
+	auto bitmap_bit = bitmap_index % 8;
+
+	assert(block_is_free(ptr, szc));
+
+	m_bitmap[bitmap_byte] |= (1 << bitmap_bit);
+}
+
+template <size_t PageSize, size_t MinAllocSize>
+bool BuddyManager<PageSize, MinAllocSize>::BuddyManagerImpl::block_is_free(void *ptr, SizeClass szc)
+{
+	auto bitmap_index = get_bitmap_index(ptr, szc);
+	auto bitmap_byte = bitmap_index / 8;
+	auto bitmap_bit = bitmap_index % 8;
+
+	return m_bitmap[bitmap_byte] & (1 << bitmap_bit);
 }
